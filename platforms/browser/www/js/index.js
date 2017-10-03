e1e507781520e69fd10baf20f080af143df853d7
Fabulej <fabulej@gmail.com>
2017-10-03 11:59:03 +0200

    add more form fields with implementation in js, fix jqm and js tags to work properly, add validateForm function

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,9 +1,22 @@
 var beerList = new Array();
 
-$ (document).ready(function(){
+function validateForm(toValidate){
+    if (toValidate != ''){
+        return toValidate;
+    } else {
+        return 'brak informacji';
+    }
+}
+
+$(document).ready(function(){
     var $newBeerInput = $('#newBeerInput');
     var $ibu = $('#ibu');
     var $beerList = $('#beerList');
+    var $abv = $('#abv');
+    var $brewery = $('#brewery');
+    var $style = $('#style');
+    
+    
     
     if( window.localStorage ){
         beerList = JSON.parse(window.localStorage.getItem('beerList'));
@@ -12,7 +25,7 @@ $ (document).ready(function(){
     
     if(null != beerList){
         for(i=0;i<beerList.length;i++){
-            var newBeer = '<div data-role="collapsible" data-key="' + beerList[i].key + '"><h4>' + beerList[i].beer  + '</h4><p>' + beerList[i].ibu + '</p></div>';
+            var newBeer = '<div data-role="collapsible" data-key="' + validateForm(beerList[i].key) + '"><h4>' + beerList[i].beer + '</h4><p>IBU: ' + validateForm(beerList[i].ibu) + '</p><p>ABV: ' + validateForm(beerList[i].abv) + '</p><p>Browar: ' + validateForm(beerList[i].brewery) + '</p><p>Styl: ' + validateForm(beerList[i].style) + '</p></div>';
             $beerList.append(newBeer);
         }
     } else  {
@@ -21,9 +34,9 @@ $ (document).ready(function(){
     
     $('#addNewBeer').on('click', function(){
        var key = Date.now();
-       var newBeer = '<div data-role="collapsible" data-key="' + key +'"><h4>' + $newBeerInput.val() + '</h4><p>' + $ibu.val() + '</p></div>';
+       var newBeer = '<div data-role="collapsible" data-key="' + key +'"><h4>' + $newBeerInput.val() + '</h4><p>IBU: ' + validateForm($ibu.val()) + '</p><p>ABV: ' + validateForm($abv.val()) + '</p><p>Browar: ' + validateForm($brewery.val()) + '<p>Styl: ' + validateForm($style.val()) + '</p></div>';
         $beerList.append(newBeer);
-        beerList.push({key:key, beer:$newBeerInput.val(), ibu:$ibu.val()});
+        beerList.push({key:key, beer:$newBeerInput.val(), ibu:$ibu.val(), abv:$abv.val(), brewery:$brewery.val(), style:$style.val()});
         if (window.localStorage){
             window.localStorage.setItem('beerList', JSON.stringify(beerList));
         }

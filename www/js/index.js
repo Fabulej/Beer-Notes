6e4c8186081fa19dd17e022cba9e2b2247e2fee0
Fabulej <fabulej@gmail.com>
2017-10-26 00:11:19 +0200

    add firebase database basic funtionality

--- www/js/index.js
+++ www/js/index.js
@@ -17,12 +17,39 @@ $(document).ready(function(){
     var $style = $('#style');
     var $plato = $('#plato');
     var $rate = $('#rate');
+    var ref = firebase.database().ref('beers');
+    ref.on('value', gotData, errData);
+    
+    function gotData(data) {
+        var beers = data.val();
+        var keys = Object.keys(beers);
+        console.log(keys);
+        for (var i = 0; i < keys.length; i++) {
+            var k = keys[i];
+            var beer = beers[k].beer;
+            var ibu = beers[k].ibu;
+            var abv = beers[k].abv;
+            var style = beers[k].style;
+            var plato = beers[k].plato;
+            var beer = beers[k].beer;
+            var rate = beers[k].rate;
+            var brewery = beers[k].brewery;
+            console.log(beer, ibu, style);
+            
+        }
+    }
     
     
+    function errData(err) {
+    console.log(err);
+    }
+        
     
     if( window.localStorage ){
         beerList = JSON.parse(window.localStorage.getItem('beerList'));
     }
+    
+    
     
     
     if(null != beerList){
@@ -41,6 +68,7 @@ $(document).ready(function(){
         } else {
            var newBeer = '<div data-role="collapsible" data-collapsed-icon="arrow-d" data-key="' + key +'"><h4>' + $newBeerInput.val() + '</h4><p>IBU: ' + validateForm($ibu.val()) + '</p><p>ABV: ' + validateForm($abv.val()) + '</p><p>Browar: ' + validateForm($brewery.val()) + '</p><p>Styl: ' + validateForm($style.val()) + '</p><p>Plato: ' + validateForm($plato.val()) + '</p><p>Ocena: ' + validateForm($rate.val()) + '</p></div>';
             $beerList.append(newBeer);
+            ref.push({key:key, beer:$newBeerInput.val(), ibu:$ibu.val(), abv:$abv.val(), brewery:$brewery.val(), style:$style.val(), plato:$plato.val(), rate:$rate.val()});
             beerList.push({key:key, beer:$newBeerInput.val(), ibu:$ibu.val(), abv:$abv.val(), brewery:$brewery.val(), style:$style.val(), plato:$plato.val(), rate:$rate.val()});
             if (window.localStorage){
                 window.localStorage.setItem('beerList', JSON.stringify(beerList));

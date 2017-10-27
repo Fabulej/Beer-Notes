61b0861bec004510f99f3dcf79ae1652a79f1aaf
Fabulej <fabulej@gmail.com>
2017-10-27 15:14:49 +0200

    mobile keyboard bugfix

--- www/js/index.js
+++ www/js/index.js
@@ -31,6 +31,11 @@ $(document).ready(function(){
     var ref = firebase.database().ref('beers');
     ref.on('value', gotData, errData);
     
+    $('input').on('focus', function(e) {
+    e.preventDefault(); e.stopPropagation();
+    window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
+});
+    
     function gotData(data) {
     beerList = null;
         var beers = data.val();
